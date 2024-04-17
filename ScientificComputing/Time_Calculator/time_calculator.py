b34ddaabc09982a997a00509b10f8a4b3c6a6aa7
Alessandro <Kinshale.world@yahoo.com>
2024-04-17 11:41:20 +0200

    Completed Time_Calculator

--- ScientificComputing/Time_Calculator/time_calculator.py
+++ ScientificComputing/Time_Calculator/time_calculator.py
@@ -1,4 +1,4 @@
-days_of_the_week = ['Monday', 'Tuesday', 'Wednsday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 def find_day(a, e):
     try:
@@ -15,33 +15,25 @@ def add(h1, h2, m1, m2):
         m % 60
     )
 
-def add_time(start, duration, dow = ''):
+def add_time(start, duration, dow = ""):
 
-    cp1 = start.index(':')
-    cp2 = duration.index(':')
-    PM = start[-2:] == 'PM'
+    cp1 = start.index(":")
+    cp2 = duration.index(":")
+    PM = start[-2:] == "PM"
 
     days, hours, mins = add(int(start[:cp1]) + PM * 12, int(duration[:cp2]), int(start[cp1 + 1: -3]), int(duration[cp2 + 1:]))
     i_dow = find_day(days_of_the_week, dow.lower().capitalize())
 
-    s1 = f'{hours if hours <= 12 else hours - 12}:{mins} {'AM' if hours <= 12 else 'PM'}'
-    s2 = '' if i_dow == -1 else f', {days_of_the_week[i_dow]}'
-    s3 =  f'{'' if days == 0 else ' (next day)' if days == 1 else f' ({days} days later)'}'
+    if (hours == 0): # handle the fact that 00:03 should be displayed as 12:03 AM
+        hours = 24
+
+    s1 = ("0" if hours < 10 else "") + f"{hours if hours <= 12 else hours - 12}" + ":" + ("0" if mins < 10 else "") + f"{mins} " + ("AM" if hours < 12 or hours == 24 else "PM")
+    s2 = "" if i_dow == -1 else f", {days_of_the_week[(i_dow + days) % 7]}"
+    s3 = "" if days == 0 else " (next day)" if days == 1 else f" ({days} days later)"
 
     return s1 + s2 + s3
 
+print(add_time('2:59 AM', '24:00', 'saturDay'))
+print(add_time('11:55 AM', '3:12'))
+print(add_time('8:16 PM', '466:02'))
 
-#Remember to add the project to GitHub
-# Manage 12 PM exception 
-
-print(add_time('11:43 AM', '00:20'))
-# Returns: 12:03 PM
-
-print(add_time('10:10 PM', '3:30'))
-# Returns: 1:40 AM (next day)
-
-print(add_time('11:43 PM', '24:20', 'tueSday'))
-# Returns: 12:03 AM, Thursday (2 days later)
-
-print(add_time('6:30 PM', '205:12'))
-# Returns: 7:42 AM (9 days later)
