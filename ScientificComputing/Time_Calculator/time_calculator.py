9286d5ab9baaff96ca725f57ae3fbcb920d1c146
Alessandro <Kinshale.world@yahoo.com>
2024-04-17 11:42:53 +0200

    Deleted print statements

--- ScientificComputing/Time_Calculator/time_calculator.py
+++ ScientificComputing/Time_Calculator/time_calculator.py
@@ -27,13 +27,9 @@ def add_time(start, duration, dow = ""):
     if (hours == 0): # handle the fact that 00:03 should be displayed as 12:03 AM
         hours = 24
 
-    s1 = ("0" if hours < 10 else "") + f"{hours if hours <= 12 else hours - 12}" + ":" + ("0" if mins < 10 else "") + f"{mins} " + ("AM" if hours < 12 or hours == 24 else "PM")
+    s1 = f"{hours if hours <= 12 else hours - 12}" + ":" + ("0" if mins < 10 else "") + f"{mins} " + ("AM" if hours < 12 or hours == 24 else "PM")
     s2 = "" if i_dow == -1 else f", {days_of_the_week[(i_dow + days) % 7]}"
     s3 = "" if days == 0 else " (next day)" if days == 1 else f" ({days} days later)"
 
     return s1 + s2 + s3
-
-print(add_time('2:59 AM', '24:00', 'saturDay'))
-print(add_time('11:55 AM', '3:12'))
-print(add_time('8:16 PM', '466:02'))
 
